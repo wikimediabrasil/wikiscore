49a0a3219f09616602067943d8d4e8b258306365
albertoleoncio <albertoleoncio@hotmail.com>
2024-11-19 14:07:54 -0300

    fix: Update evaluation handling to use fetched diff object

--- contests/handlers/triage.py
+++ contests/handlers/triage.py
@@ -186,12 +186,13 @@ class TriageHandler:
         )
 
         for locked in lockeds:
+            diff = Edit.objects.get(contest=self.contest, diff=locked.diff)
             evaluation = Evaluation.objects.create(
                 contest=self.contest,
-                diff=locked.diff,
+                diff=diff,
                 status='0' # Status '0' indicates the edit is pending
             )
-            Edit.objects.filter(contest=self.contest, diff=locked.diff).update(last_evaluation=evaluation)
+            diff.update(last_evaluation=evaluation)
 
         return lockeds
 
