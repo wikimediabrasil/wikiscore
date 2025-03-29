c2ff80e420a1d75fc92734959007d4f1b2e284e8
albertoleoncio <albertoleoncio@hotmail.com>
2025-03-29 13:02:26 -0300

    fix: Add import for requests and PermissionDenied in ManageHandler

--- contests/handlers/manage.py
+++ contests/handlers/manage.py
@@ -1,5 +1,7 @@
+import requests
 from contests.models import Contest, Evaluator, Edit, Participant, Article
 from credentials.models import Profile
+from django.core.exceptions import PermissionDenied
 
 class ManageHandler():
     def execute(self, request):
